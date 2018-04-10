c1f7012fec07f4ca652e85428d3c2700db060b8f
Sergey Komissarov <serg.kom@gmail.com>
2018-04-10 23:54:22 +0300

    gems commit

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,13 @@
 class IndexController < ApplicationController
+  require 'nokogiri'
+  require 'open-uri'
+
   def hello
+    url = 'http://mychamp.ru/'
+    html = open(url)
+    doc = Nokogiri::HTML(html)
+    doc.encoding = 'utf-8'
+    binding.pry
+    b=1
   end
 end
